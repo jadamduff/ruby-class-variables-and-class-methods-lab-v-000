124246b77f5243f1060c5abd06a1e2b76ed2544b
Adam Duff <j.adam.duff@gmail.com>
2018-08-15 05:41:41 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name)
+    @name = name
+    @@count += 1
+    @@artists << self.artist
+    @@genres << self.genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genres
+    @@genres
+  end
+
+  
+end
