d354652c8e581a2a7b51285938f1252ba3a4d693
Adam Duff <j.adam.duff@gmail.com>
2018-08-15 05:44:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artists << self.artist
+    @@artists << self.artist if !@@artists.include?(self.artist)
     @@genres << self.genre
   end
 
